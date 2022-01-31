5b1967ab5ca89f61fbc296507293c4527ad672f2
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-01-30 19:25:28 -0500

    Initial Commit for the compiler repo

---
+++ src/compiler.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class compiler {
+    public static void main(String[] args) {
+        System.out.println("I cannot wait to have a fully functional compiler I made myself!");
+    }
+}
