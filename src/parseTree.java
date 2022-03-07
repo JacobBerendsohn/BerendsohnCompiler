e4cba5e6dde7c07b527d0eaf08b8f09771cdc877
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-03-07 13:23:34 -0500

    Changed node and parsetree to create nodes the right way, without 4 constructors lol

--- src/parseTree.java
+++ src/parseTree.java
@@ -3,18 +3,10 @@ public class parseTree {
     node rootNode = null;
     node currentNode = null;
 
-    public void addNode(String kind, String label, Boolean isLeaf) {
+    public void addNode(String label, Boolean isLeaf) {
         // Creating the node to be added to the tree and naming it
-        node curParseNode = new node();
+        node curParseNode = new node(label);
         curParseNode.name = label;
-
-        // Setting the root node and the parent node for a current tree
-        if (this.rootNode != null) {
-            curParseNode.parent = currentNode;
-        } else {
-            this.rootNode = curParseNode;
-            curParseNode.parent = null;
-        }
 
         // Check to see if the node is a leaf node
         if (isLeaf) {
