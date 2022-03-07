17b2eae89d431541e11522eb6a3346094fc986d6
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-03-07 14:56:48 -0500

    Added all non-terminal functions to parse and created match function

--- src/parser.java
+++ src/parser.java
@@ -1,12 +1,112 @@
+import java.util.ArrayList;
+
 public class parser {
     // Main parse function
-    
-    public parseTree parse() {
+    parseTree currTree = new parseTree();
+    ArrayList<token> tokens = new ArrayList<token>();
+    int currTokenInArray = 0;
 
-        // Replace with the real parse tree that will be sent to semantic analysis
-        return new parseTree();
+    public void startParse(ArrayList<token> tokenList) {
+        this.tokens = tokenList;
+        parseProgram();
     }
 
-    // Add a function to parse each non-terminal in the grammar
+    public void parseProgram() {
+        currTree.addNode("Program", false);
+        parseBlock();
+        match("$");
+        currTree.executeOrder66();
+    }
 
+    public void match(String expected) {
+        if (expected.equals(tokens.get(currTokenInArray).getValue())) {
+            currTree.addNode(expected, true);
+        } else {
+            // Add error handling
+        }
+    }
+
+    public void parseBlock() {
+
+    }
+
+    public void parseStatementList() {
+
+    }
+
+    public void parseStatement() {
+
+    }
+
+    public void parsePrintStatement() {
+
+    }
+
+    public void parseAssignmentStatement() {
+
+    }
+
+    public void parseVarDecl() {
+
+    }
+
+    public void parseWhileStatement() {
+
+    }
+
+    public void parseIfStatement() {
+
+    }
+
+    public void parseExpr() {
+
+    }
+
+    public void parseIntExpr() {
+
+    }
+
+    public void parseStringExpr() {
+
+    }
+
+    public void parseBooleanExpr() {
+
+    }
+
+    public void parseID() {
+
+    }
+
+    public void parseCharList() {
+
+    }
+
+    public void parseType() {
+
+    }
+
+    public void parseChar() {
+
+    }
+
+    public void parseSpace() {
+
+    }
+
+    public void parseDigit() {
+
+    }
+
+    public void parseBoolOp() {
+
+    }
+
+    public void parseBoolVal() {
+
+    }
+
+    public void parseIntOp() {
+
+    }
 }
