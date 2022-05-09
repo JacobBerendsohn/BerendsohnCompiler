fdc079df854f1a00b88cfd942bb5967af5cb2adc
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-05-09 14:07:04 -0400

    Creation of the 256bytes of data, held in an array of strings

--- src/codeGen.java
+++ src/codeGen.java
@@ -1,3 +1,8 @@
 public class codeGen {
+    // Creation of 256 bytes of data, held in an array of strings
+    String[] data = new String[255];
 
+    public String[] generateCode() {
+        return data;
+    }
 }
